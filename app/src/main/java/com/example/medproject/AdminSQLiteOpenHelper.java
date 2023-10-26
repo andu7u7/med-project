9dbe6d5086c63078927f12feffae463435fd1a2f
Andy Rojas <ar0330yt@gmail.com>
2023-10-26 01:14:01 -0500

    AdminSQLiteOpenHelper agregado. Guardar datos en bd.

---
+++ app/src/main/java/com/example/medproject/AdminSQLiteOpenHelper.java
@@ -0,0 +1,24 @@
+package com.example.medproject;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE medicamentos (id INTEGER PRIMARY KEY, nombre TEXT NOT NULL, dosis INTEGER NOT NULL CHECK (dosis > 0),intervalo INTEGER NOT NULL, fecha_inicio TEXT NOT NULL, hora_inicio TEXT NOT NULL, fecha_fin TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/com/example/medproject/MainActivity.java
+++ app/src/main/java/com/example/medproject/MainActivity.java
@@ -2,13 +2,85 @@ package com.example.medproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegresar, btnFechaInicio, btnHoraInicio, btnFechaFin, btnRegistrar;
+    EditText txtNombreMedicamento, txtHoraInicio, txtFechaInicio, txtIntervalo, txtDosis, txtFechaFin;
+    RadioGroup rgIntervalo;
+    RadioButton rbIntervaloDias, rbIntervaloHoras;
+    CheckBox cbCronico;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtNombreMedicamento = findViewById(R.id.txtNombreMedicamento);
+        txtHoraInicio = findViewById(R.id.txtHoraInicio);
+        txtFechaInicio = findViewById(R.id.txtFechaInicio);
+        txtIntervalo = findViewById(R.id.txtIntervalo);
+        rgIntervalo = findViewById(R.id.rgIntervalo);
+        rbIntervaloDias = findViewById(R.id.rbIntervaloDias);
+        rbIntervaloHoras = findViewById(R.id.rbIntervaloHoras);
+        txtDosis = findViewById(R.id.cantDosis);
+        txtFechaFin = findViewById(R.id.txtFechaFin);
+        cbCronico = findViewById(R.id.cbCronico);
+    }
+
+    public void registrar(View view) {
+        try {
+            AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "med_project.db", null, 1);
+            SQLiteDatabase bd = admin.getWritableDatabase();
+            String nombreMedicamento = txtNombreMedicamento.getText().toString();
+            String horaInicio = txtHoraInicio.getText().toString();
+            String fechaInicio = txtFechaInicio.getText().toString();
+            int intervalo = Integer.parseInt(txtIntervalo.getText().toString());
+            int dosis = Integer.parseInt(txtDosis.getText().toString());
+            String fechaFin = txtFechaFin.getText().toString();
+
+            if (rbIntervaloDias.isChecked()) {
+                intervalo *= 24;
+            }
+            if (cbCronico.isChecked()) {
+                fechaFin = null;
+            }
+
+            ContentValues registro = new ContentValues();
+            registro.put("nombre", nombreMedicamento);
+            registro.put("dosis", dosis);
+            registro.put("hora_inicio", horaInicio);
+            registro.put("fecha_inicio", fechaInicio);
+            registro.put("fecha_fin", fechaFin);
+            registro.put("intervalo", intervalo);
+            bd.insert("medicamentos", null, registro);
+            bd.close();
+
+            Toast.makeText(getApplicationContext(), "Registro correcto!", Toast.LENGTH_LONG).show();
+            // Limpiar();
+        } catch (Exception e) {
+            String error = e.getMessage();
+            Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void Limpiar() {
+        txtNombreMedicamento.setText("");
+        txtHoraInicio.setText("");
+        txtFechaInicio.setText("");
+        txtIntervalo.setText("");
+        txtDosis.setText("");
+        txtFechaFin.setText("");
     }
 }
